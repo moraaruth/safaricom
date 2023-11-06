69c59f52a5adbd97502715cb80ea4e2f591cc283
Ruth Moraa <moraar37@gmail.com>
2023-11-06 05:08:03 +0300

    floating navbar to the right and adding margin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,64 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import './Style.css'
+import Button from '@mui/material/Button';
+import ShareIcon from '@mui/icons-material/Share';
 
+import './Style.css';
 
 function Navbar() {
+  const customButtonStyle = {
+    backgroundColor: 'rgb(15, 131, 15)',
+    color: 'white',
+    borderRadius: '50%',
+    minWidth: '3.0rem',
+    minHeight: '3.0rem',
+    padding: '0.5rem',
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/">Safaricom</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">Home</a>
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <a className="nav-link mx-4" href="/">Home</a> 
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/about">Discover</a>
+            <a className="nav-link mx-4" href="/about">Discover</a> 
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/contact">Exprience More</a>
+            <a className="nav-link mx-4" href="/contact">Experience More</a> 
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/contact">Testimonials</a>
+            <a className="nav-link mx-4" href="/testimonials">Testimonials</a> 
           </li>
-          <button type="button" className='signin' >
-           Sign in
-          </button>
+          <li className="nav-item">
+            <button type="button" className="btn btn-primary signin mx-2">Sign in</button>
+          </li>
+          <li className="nav-item mx-4">
+            <Button
+              variant="contained"
+              sx={customButtonStyle}
+              startIcon={<ShareIcon />}
+            >
+             
+            </Button>
+          </li>
         </ul>
       </div>
     </nav>
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
