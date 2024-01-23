08a6c02d4f2f1eef8740ea7b13c320efe2c404e4
Ruth Moraa <moraar37@gmail.com>
2024-01-23 04:18:47 +0300

    adding dasboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { useState, UseEffect } from "react";
+
+function Dashboard() {
+
+    const [accoutBalances, setAccountBalances] = useState({});
+    const [appInfo, setAppInfo] = useState('');
+
+    return ( 
+        <div>
+            
+        </div>
+
+    )
+}
+export default Dashboard
