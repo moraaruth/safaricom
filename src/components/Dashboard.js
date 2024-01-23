8ecaba5ac539b22b96a5cc867dee83385d5472ec
Ruth Moraa <moraar37@gmail.com>
2024-01-23 04:27:13 +0300

    using fetch and adding dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,28 @@
-import React, { useState, UseEffect } from "react";
+import React, { useState, UseEffect, useEffect } from "react";
 
 function Dashboard() {
 
     const [accoutBalances, setAccountBalances] = useState({});
     const [appInfo, setAppInfo] = useState('');
 
+    useEffect(() => {
+        fetch('')
+        .then(response => response.json())
+        .then(data => setAccountBalances(data))
+        .catch(error => console.error('Error fetching account balances:', error ))
+    })
+
     return ( 
         <div>
-            
+            <h1>Safaricom Dashboard</h1>
+
+            <div>
+                <h2>Your Account Balances</h2>
+                <p>Airtime: {accountBalances.airtime || 'Loading...'}</p>
+                <p>SMS: {accountBalances.sms || 'Loading...'}</p>
+                <p>Data: {accountBalances.data || 'Loading...'}</p>
+            </div>
+
         </div>
 
     )
