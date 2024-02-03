363ea06e8a984207871f2196cfb48d21ab171af2
Ruth Moraa <moraar37@gmail.com>
2024-02-03 09:10:35 +0300

    adjusting account component location

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,11 +4,11 @@ function Account() {
     return (
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
 
-            <div style={{ flex: '1', marginRight: '1rem' }}>
+            <div style={{ flex: '1', marginRight: '1rem', marginTop:'25%' }}>
 
-            <h2 style={{ textAlign: 'left', marginLeft: '7.5rem' }}>Manage Your Safaricom <br />Account</h2>
+            <h2 style={{ textAlign: 'left', marginLeft: '9.5rem' }}>Manage Your Safaricom <br />Account</h2>
             <br />
-            <p style={{ textAlign: 'left', marginLeft: '7.5rem' }}>
+            <p style={{ textAlign: 'left', marginLeft: '9.5rem' }}>
             Manage your Postpay or Prepay account here at Safaricom.com.<br /> Check you Credit limits, 
             pay your bills, manage your Bonga Points <br /> and so much more.
             </p>
