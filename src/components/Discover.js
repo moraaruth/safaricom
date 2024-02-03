0271175a13a87deeb800626ccb5d6873d26a927b
Ruth Moraa <moraar37@gmail.com>
2024-02-03 08:32:47 +0300

    styling the discover componet

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -6,15 +6,18 @@ function Discover() {
         <>
             <h3>Discover Safaricom World</h3>
             <div className='container'>
-                <div className='content'>
-                    <h6>Get to read your favourite Newspaper publication</h6>
-                    <p>Right from Safaricom News, do your shopping at Masoko, Enjoy your favourite videos, music and so much more on Safaricom World.</p>
-                    <img src="reading-newspaper.webp" alt="reading newspaper Logo" />
+                <div className='content' style={{ position: 'relative' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                        <h6>Get to read your favourite Newspaper publication</h6>
+                        <p>Right from Safaricom News, do your shopping at Masoko, Enjoy your favourite videos, music and so much more on Safaricom World.</p>
+                    </div>
+                    <img className='newspaper-image' src="reading-newspaper.webp" alt="reading newspaper Logo" style={{ position: 'absolute', bottom: 0, right: 0 }} />
                 </div>
+
                 <div className='content'>
                     <h6>View and analyse your spending</h6>
                     <p>Get a detailed breakdown of the usage of your bundles, track and manage the most consuming apps.</p>
-                    <img src="stats.webp"  alt='stats'/>
+                    <img src="stats.webp" alt='stats' />
                 </div>
             </div>
         </>
