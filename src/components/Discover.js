bbfd1b3470a59919dc6d76eddb2724edad128f32
Ruth Moraa <moraar37@gmail.com>
2024-02-03 08:56:57 +0300

    styling the exprience component

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -6,7 +6,7 @@ function Discover() {
         <>
             <h3>Discover Safaricom World</h3>
             <div className='container'>
-                <div className='content' style={{ position: 'relative' }}>
+                <div className='content-discover' style={{ position: 'relative' }}>
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
                         <h6>Get to read your favourite Newspaper publication</h6>
                         <p>Right from Safaricom News, do your shopping at Masoko, Enjoy your favourite videos, music and so much more on Safaricom World.</p>
@@ -14,7 +14,7 @@ function Discover() {
                     <img className='newspaper-image' src="reading-newspaper.webp" alt="reading newspaper Logo" style={{ position: 'absolute', bottom: 0, right: 0 }} />
                 </div>
 
-                <div className='content'  style={{ position: 'relative' }}>
+                <div className='content-discover'  style={{ position: 'relative' }}>
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
                     <h6>View and analyse your spending</h6>
                     <p>Get a detailed breakdown of the usage of your bundles, track and manage the most consuming apps.</p>
--- src/components/ExprienceMore.js
+++ src/components/ExprienceMore.js
@@ -13,22 +13,35 @@ function ExprienceMore() {
                 <div className='container'>
 
                     <div className='content'>
-                        <h6 style={{ fontSize: '22px', fontWeight: '450' }}>View M-PESA statement</h6><br />
-                        <img src='mpesa-icon.2ba5d6c9.svg' alt='m-pesa'/>
+                        <div className='exprience-container'>
+                            <img className='exprienceImage' src='mpesa-icon.2ba5d6c9.svg' alt='m-pesa' />
+                            <h6 style={{ fontSize: '22px', fontWeight: '450' }}>View M-PESA statement</h6><br />
+
+                        </div><br />
                         <p>This is a DIY (Do-It-Yourself) service that enables you to access an e-stamped M-PESA statements on demand without the need to visit a Safaricom Retail Shop.</p>
+
                         <br /><br />
-                        <img src='data-sharing-icon.386b4646.svg' alt='data sharing' />
-                        <h6 style={{ fontSize: '22px', fontWeight: '450' }}>Data Sharing</h6><br />
+                        <div className='exprience-container'>
+                            <img className='exprienceImage' src='data-sharing-icon.386b4646.svg' alt='data sharing' />
+                            <h6 style={{ fontSize: '22px', fontWeight: '450' }}>Data Sharing</h6><br />
+                        </div><br />
                         <p>A convenient and hassle free Data sharing solution. You can add up to six numbers to your line. Afterward, you can choose how much data each number should access from your line, ranging from 10MBs to 1GB.</p>
+
                     </div><br />
                     <div className='content'>
-                        <img src='tunukiwa-offers-icon.85475bdf.svg' alt='tunukiwa' />
-                        <h6 style={{ fontSize: '22px', fontWeight: '450' }}>Tunukiwa Offers</h6><br />
+                        <div className='exprience-container'>
+                            <img className='exprienceImage' src='tunukiwa-offers-icon.85475bdf.svg' alt='tunukiwa' />
+                            <h6 style={{ fontSize: '22px', fontWeight: '450' }}>Tunukiwa Offers</h6><br />
+                        </div><br />
                         <p>We have deals for everyone, everyday. Get personalised offers that give you access to the products, services and bundles you will love, at prices you will love. Go on - check what offers and discounts you can receive right NOW with Tunukiwa.</p>
+
                         <br />
-                        <img src='discover-more-icon.b13469aa.svg' alt='discover more'/>
-                        <h6 style={{ fontSize: '22px', fontWeight: '450' }}>Discover More Lifestyle</h6><br />
+                        <div className='exprience-container'>
+                            <img className='exprienceImage' src='discover-more-icon.b13469aa.svg' alt='discover more' />
+                            <h6 style={{ fontSize: '22px', fontWeight: '450' }}>Discover More Lifestyle</h6><br />
+                        </div><br />
                         <p>The one stop destination for all things Safaricom. Manage your account, buy bundles, view our latest promotions, pay your bills, get exclusive access to Music, Movies and educational platforms, PLUS so much MORE!</p>
+
                     </div>
                 </div>
             </div>
